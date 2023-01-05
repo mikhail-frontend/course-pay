a551758e50d88a1e97471429803b27eedfb90bde
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2023-01-05 12:24:45 +0300

    small fixing

--- hooks/useAnimation.ts
+++ hooks/useAnimation.ts
@@ -1,16 +1,16 @@
 import React, {useCallback, useEffect, useRef, useState} from "react";
 
-export default function useAnimation<T>(list: T[], setList: React.Dispatch<React.SetStateAction<T[]>>): React.RefObject<HTMLUListElement> | null {
-    const [animated, setAnimated]= useState<boolean>(false);
+export default function useAnimation<T>(list: T[], setList: React.Dispatch<React.SetStateAction<T[]>>): React.RefObject<HTMLUListElement>  {
+    const [animated, setAnimated] = useState<boolean>(false);
     const wrapRef = useRef<HTMLUListElement>(null)
 
     const setElementIsAnimated:React.EffectCallback = useCallback(() => {
-        const globalCopy:T[] = [...list];
+        const globalCopy:T[] = list.slice();
         globalCopy.forEach((item, index) => {
             const timer:number = (index + 1) * 70;
             setTimeout(() => {
                 setList((prevValue) => {
-                    const copy = [...prevValue];
+                    const copy = prevValue.slice();
                     copy[index] = {...copy[index], animated: true};
                     return copy
                 })
@@ -19,7 +19,7 @@ export default function useAnimation<T>(list: T[], setList: React.Dispatch<React
     }, [list, setList]);
 
     useEffect(() => {
-        const target: Element | null = (wrapRef)!.current;
+        const target: Element | null = (wrapRef)?.current;
         const handleIntersection = (entries: IntersectionObserverEntry[]):void => {
             if (animated) return;
             for (let entry of entries) {
